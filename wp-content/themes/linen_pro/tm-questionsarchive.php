a9878df2fe3db21ac1f12943cd61311eaf9ef3d0
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-06 16:19:05 +0100

    Removed Anju's posts from questions archive

--- wp-content/themes/linen_pro/tm-questionsarchive.php
+++ wp-content/themes/linen_pro/tm-questionsarchive.php
@@ -14,15 +14,32 @@ Template Name: Questions archive
 			<ul>
 				<?php while (have_posts()) : the_post(); ?>
 
-						<?php foreach (get_comments() as $comment): ?>
-						<li><?php echo $comment->comment_author; ?> said: <a href="<?php echo get_comment_link( $comment->comment_ID ); ?>"> <?php echo get_comment_excerpt( $comment->comment_ID );  ?>.</a></li>
-						<?php endforeach; ?>
+				<?php
 
+				// WP_Comment_Query arguments
+				$args = array (
+					'author__in'   => array('Anju Sinha'),
+
+				);
+
+				// The Comment Query
+				$comment_query = new WP_Comment_Query;
+				$comments = $comment_query->query( $args );
+
+				// Comment Loop
+				if ( $comments ) {
+					foreach ( $comments as $comment ) { ?>
+						<li><?php echo $comment->comment_author; ?> said: <a href="<?php echo get_comment_link( $comment->comment_ID ); ?>"> <?php echo get_comment_excerpt( $comment->comment_ID );  ?>.</a></li>
+					<?php }
+				} else {
+					echo 'No comments found.';
+				}
+				?>
+				
 				<?php endwhile; ?>
 
-				<?php echo paginate_links( $args ); ?>
-
 				<?php endif; ?>
+
 			</ul>
 		</div><!--end entries-->
 
